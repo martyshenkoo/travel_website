2dbcdf8f5a6be7a462ea5c7c4c0f857ac8b6ea32
Iryna <martyshenko.i.s@gmail.com>
2025-03-10 18:51:33 +0100

    Add explore section and also fixed navigation bar

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function () {
+  const navigationBar = document.querySelector('.navigation');
+  if (window.scrollY > 50) {
+    navigationBar.classList.add('scrolled');
+  } else {
+    navigationBar.classList.remove('scrolled');
+  }
+});
