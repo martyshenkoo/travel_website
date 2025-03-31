d11ca574ffeca5dd28353c1adce5c20e63c5437d
Iryna <martyshenko.i.s@gmail.com>
2025-03-31 16:20:45 +0200

    lab 7

--- script.js
+++ script.js
@@ -6,3 +6,64 @@ window.addEventListener('scroll', function () {
     navigationBar.classList.remove('scrolled');
   }
 });
+
+
+function changeButtonColor() {
+  document.querySelector('.explore-button').style.backgroundColor = 'red';
+}
+
+const button = document.querySelector('.explore-button');
+button.onmouseout = function () {
+  button.style.backgroundColor = '';
+};
+
+
+const logo = document.querySelector('.logo');
+
+function changeText() {
+  logo.textContent = 'TRAVEL WORLD';
+}
+
+function changeTextBack() {
+  logo.textContent = 'TRAVEL';
+}
+
+logo.addEventListener('mouseover', changeText);
+logo.addEventListener('mouseout', changeTextBack);
+
+
+const formHandler = {
+  handleEvent(event) {
+    if (event.type === 'submit') {
+      event.preventDefault();
+      alert('Форма відправлена!');
+    }
+  }
+};
+
+document.querySelector('.cta-form').addEventListener('submit', formHandler);
+
+
+document.getElementById('list').addEventListener('click', function (event) {
+  if (event.target.tagName === 'LI') {
+    document.querySelectorAll('#list li').forEach(item => item.classList.remove('active'));
+    event.target.classList.add('active');
+  }
+});
+
+document.getElementById('menu').addEventListener('click', function (event) {
+  if (event.target.tagName === 'BUTTON') {
+    let action = event.target.dataset.action;
+    let list = document.getElementById('list');
+
+    if (action === 'add') {
+      let newItem = document.createElement('li');
+      newItem.textContent = `Application number ${list.children.length + 1}`;
+      list.appendChild(newItem);
+    } else if (action === 'remove') {
+      if (list.lastElementChild) list.removeChild(list.lastElementChild);
+    } else if (action === 'clear') {
+      list.innerHTML = '';
+    }
+  }
+});
